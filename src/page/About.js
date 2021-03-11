8f8e5253f6fbe76faaeee1665d161abd847e327e
031hoodboy <littlepark0107@gmail.com>
2021-03-11 15:33:10 +0900

    Feat: Pages publishing

--- src/page/About.js
+++ src/page/About.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import Header from '../Componenet/HomeHeader';
 import styled from 'styled-components';
-import Header from '../Componenet/Header';
+import LogoImg from '../images/logo.png';
 
-const Test = styled.div`
-    width: 100vw;
+const Wrapper = styled.div`
+    margin: 0 auto;
     height: 100vh;
-    background: yellow;
+    width: 85vw;
+    align-items: center;
+    justify-content:center;
+    display: flex;
+    flex-direction: column;
 `;
 
-function About() {
+const Span = styled.div`
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 24px;
+    font-weight: bold;
+    
+`;
+
+const Logo = styled.div`
+    width: 55vw;
+    height: 200px;
+    background-image: url(${LogoImg});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;    
+    margin: 10vh 0;
+`;
+
+function About () {
     return (
         <>
-            <Header/>
-            <Test/>
+        <Header/>
+        <Wrapper>
+            <Logo/>
+            <Span>
+                LIBILLY는 로컬 스트리트 문화를 기반으로 전개하는 브랜드입니다.
+                <br />
+                거리의 다양한 영감을 디자린 뿐만 아니라 사진과 영상등을 통해 표현합니다.
+            </Span>
+        </Wrapper>
         </>
     )
 }
